082f160394feba5f24efd5e60f7ab8ccd3345081
Moises <fabmoy1866@gmail.com>
2020-05-20 20:26:24 -0500

    Initial Commit

---
+++ process.sh
@@ -0,0 +1,75 @@
+
+
+##FUNCTIONS ##
+writeTittle(){
+  echo "  ___                                   __  __    _        __ "
+  echo " | _ \  _ _   ___   __   ___   ___  ___ \ \/ /   / |      /  \ "
+  echo " |  _/ | '_| / _ \ / _| / -_) (_-< (_-<  >  <    | |  _  | () |"
+  echo " |_|   |_|   \___/ \__| \___| /__/ /__/ /_/\_\   |_| (_)  \__/ "
+  echo "****************************************************************"
+}
+
+createNewProcess (){
+  echo "# WRITE THE NAME OF THE PROCESS IN ORDER TO START IT"
+  read name
+  $name &> processOutput.txt ## TRY TO RUN THE PROCESS AND THEN REDIRECT THE
+  #OUTPUT TO A FILE
+}
+
+showAllProcesses (){
+  echo "** CURRENT PROCESSES **"
+  ps -d
+  read -p "PRESS ENTER TO CONTINUE"
+}
+
+veriffyIfSU (){
+  res=$(whoami | grep root)
+  if [[ -z $res ]]; then
+    echo "YOU HAVE TO RUN THE SCRIPT AS ROOT IN ORDER TO USE IT :("
+    exit
+  fi
+}
+
+lookForP (){
+  echo "WRITE THE PROCESS NAME"
+  read name
+  echo " ************** **************"
+  res=$( ps -d | grep $name )
+  if [[ -z $res  ]]; then
+    echo "NO SE ENCONTRARON COINCIDENCIAS"
+  else
+    echo $res
+  fi
+}
+##END FUNCTIONS ##
+
+##CODE ##
+option="x"
+veriffyIfSU
+while [[ $option != Exit ]]; do
+  writeTittle
+  select option in CreateProcess KillProcess ShowProcesses LookForAProcess Exit
+    do
+      case $option in
+        CreateProcess)
+          createNewProcess
+        ;;
+
+        ShowProcesses)
+          showAllProcesses
+        ;;
+
+        LookForAProcess)
+          lookForP
+        ;;
+
+        *)
+          echo "INVALID OPTION"
+        ;;
+
+      esac
+    break
+    done
+done
+
+##END OF THE CODE ##
---
+++ test.sh
@@ -0,0 +1,6 @@
+i=0
+
+while [[ $i<5 ]]; do
+	echo $i
+	let i=$i+1
+done
